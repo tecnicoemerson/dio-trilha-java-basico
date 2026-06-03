cd465d213c276347296b40e2a2a3778a92e6c55b
tecnicoemerson <emersonrodrigues5578@gmail.com>
2026-06-03 10:54:36 -0300

    Atualizando projeto

---
+++ src/edu/emerson/tema/tiposvariaveis/TiposVariaveis.java
@@ -0,0 +1,14 @@
+package edu.emerson.tema.tiposvariaveis;
+
+public class TiposVariaveis {
+
+    public static void main(String[] args) throws Exception{
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
+        long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo
+        float pi = 3.14F;
+        double salario = 1275.33;
+        String meuNome = "Emerson Rodrigues" // representa texto na aplicaçao
+    }
+}
