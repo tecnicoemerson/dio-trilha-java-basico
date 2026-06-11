bff917152711afe8bda2997bd390618f00c2b714
tecnicoemerson <emersonrodrigues5578@gmail.com>
2026-06-11 10:58:51 -0300

    Conhecendo as estruturas de repeticao

---
+++ src/edu/emerson/temaestudados/estruturarepeticao/ExemploCarneirinhos.java
@@ -0,0 +1,10 @@
+package edu.emerson.temaestudados.estruturarepeticao;
+
+// ExemploFor
+public class ExemploCarneirinhos {
+    public static void main(String[] args) {
+        for(int carneirinhos = 1 ; carneirinhos <=20; carneirinhos ++) {
+            System.out.println(carneirinhos + " - Carneirinho(s)");
+        }
+    }
+}
---
+++ src/edu/emerson/temaestudados/estruturarepeticao/ExemploFor.java
@@ -0,0 +1,13 @@
+package edu.emerson.temaestudados.estruturarepeticao;
+
+// Exemplo For e array.java
+public class ExemploFor {
+    public static void main(String[] args) {
+        String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+
+        for (int x=0; x<alunos.length; x++) {
+            System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
+        }
+
+    }
+}
